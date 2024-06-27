90d2e49ef7eb53b286b54ed00886ac547e501ac7
bueorm <dalusx64@gmail.com>
2024-06-27 18:27:15 -0500

    Initial commit

--- scripts.js
+++
@@ -1,7 +0,0 @@
-function openPrivacyPolicy() {
-    document.getElementById("privacy-policy-modal").style.display = "block";
-}
-
-function closePrivacyPolicy() {
-    document.getElementById("privacy-policy-modal").style.display = "none";
-}
