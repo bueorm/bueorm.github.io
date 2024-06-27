3c1db08cf587c3cd8fd3bf39226978d7f922d32b
bueorm <dalusx64@gmail.com>
2024-06-27 17:33:23 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function openPrivacyPolicy() {
+    document.getElementById("privacy-policy-modal").style.display = "block";
+}
+
+function closePrivacyPolicy() {
+    document.getElementById("privacy-policy-modal").style.display = "none";
+}
